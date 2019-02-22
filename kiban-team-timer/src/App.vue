3c3e5aafd70db707d6bcba9420a4b7078920d919
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2019-02-22 14:41:49 +0900

    一通り動くようにした

--- kiban-team-timer/src/App.vue
+++ kiban-team-timer/src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    Hoge
-    <button v-on:click="doHello">Hello!!!</button>
     <Config ref="config"></Config>
 
     <table id="main-contents">
       <tr><td class="key">現在</td><td class="value">{{currentTime}}</td></tr>
-      <tr><td class="key">次のイベント</td><td class="value">{{nextEvent}}</td></tr>
+      <tr><td class="key">次のイベント</td><td class="value">{{events[nextEventIndex]}}</td></tr>
       <tr><td class="key">Future Events</td>
       <td class="value">
         <ol id="future-events-list">
-          <li v-for="ev in futureEvents">{{ev.time}} : {{ev.eventName}}</li>
+          <li v-for="ev in futureEvents()">{{ev.time}} : {{ev["event-name"]}}</li>
         </ol>
       </td></tr>
     </table>
@@ -34,25 +32,22 @@ export default {
   data: function() {
     return {
       currentTime: null,
-      nextEvent: "sample2",
-      futureEvents: [
-        { time: "aaa", eventName: "bbb" }, 
-        { time: "ccc", eventName: "ddd" }, 
-      ]
+      nextEventIndex: 0,
+      events: [{}],
     }
   },
   methods :{
-    doHello() {
-      if(timer != null){
-        clearInterval(timer);
-        timer = null;
-        return;
+    futureEvents() {
+      const res = [];
+      for (let i = this.nextEventIndex + 1; i < this.events.length; i++) {
+        res.push(this.events[i]);
       }
-      timer = setInterval(this.notify,10000);
-
+      for (let i = 0; i < this.nextEventIndex; i++) {
+        res.push(this.events[i]);
+      }
+      return res;
     },
     notify(eventName) {
-      const soundName = this.$refs.config.content
       const fileName = new Schedule().getMusic(eventName)
       new Audio(require("./assets/"+ fileName)).play();
       console.log(fileName)
@@ -60,8 +55,11 @@ export default {
   },
   created() {
     const schedule = new Schedule();
+    this.events = schedule.getAllEvents();
     Clock.createAsync(['']).then(c => {
       clock = c;
+      const now = new Date(clock.now());
+      this.nextEventIndex = schedule.getNextEventIndex(now);
       setInterval(() => {
         const now = clock.now();
         const nowDate = new Date(now);
@@ -69,38 +67,23 @@ export default {
         if (nowText != this.currentTime) {
           this.currentTime = nowText;
         }
-        const events = schedule.getNextEvents(nowDate.getHours(), nowDate.getMinutes());
-        events.map( x => {
+        for (let i = this.nextEventIndex; i < this.events.length; i++) {
+          const x = this.events[i];
           const eventTime = new Date(now);
           const splitted = x.time.split(':');
           eventTime.setHours(Number.parseInt(splitted[0]));
           eventTime.setMinutes(Number.parseInt(splitted[1]));
           eventTime.setSeconds(0);
           eventTime.setMilliseconds(0);
-          const diff = eventTime.getTime() - nowDate.getTime();
+          const diff = eventTime.getTime() - now;
           if (diff < 0) {
             console.log(diff);
             this.notify(x["event-name"]);
+            this.nextEventIndex = (this.nextEventIndex + 1) % this.events.length;
           }
-        });
+        }
       }, 100);
     });
-  },
-  mounted() {
-    console.log("mounted");
-    let schedule = new Schedule();
-    const now = new Date();
-    let events = schedule.getNextEvents(now.getHours(), now.getMinutes());
-    events.map( x => {
-      const eventTime = new Date()
-      const splitted = x.time.split(':')
-      eventTime.setHours(Number.parseInt(splitted[0]))
-      eventTime.setMinutes(Number.parseInt(splitted[1]))
-      eventTime.setSeconds(0)
-      const diff = eventTime.getTime() - now.getTime()
-      console.log(diff)
-      setTimeout(() => this.notify(x["event-name"]), diff)
-    })
   },
   beforeDestroy() {
     console.log("before destroy");
